138afd60f1a72fe41b9c68622cca3ac8d3c71983
Derek McBurney <d.mcburney@gmail.com>
2011-12-10 14:17:43 -0800

    Page image views.
    Adds view files for featured and listed images in page themes.

---
+++ ci_2.0.3_application/views/page_image.php
@@ -0,0 +1,1 @@
+<a href="<?php echo base_url().$image['urlpath'];?>" rel="lightbox[<?php echo $pageid;?>]"><img src="<?php echo base_url().$image['urlpath'];?>/280" alt="" class="featured_image"/></a>
