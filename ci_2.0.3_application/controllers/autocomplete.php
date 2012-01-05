706854113ebb891d53aff7bc3b896bc413d0bc69
Derek McBurney <d.mcburney@gmail.com>
2012-01-05 00:28:08 -0800

    Added form autocompletion.
    Autocompletion has been added for typing user display names to
    grant them special permissions on a page. This change also
    requires config to add the 'autocomplete' controller, the
    site.php to load scriptaculous with the controls library, and
    the CSS to be updated for the .autocomplete class.

---
+++ ci_2.0.3_application/controllers/autocomplete.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package		dmcb-cms
+ * @author		Derek McBurney
+ * @copyright	Copyright (c) 2011, Derek McBurney, derek@dmcbdesign.com
+ *              This code may not be used commercially without the expressed
+ *              written consent of Derek McBurney. Non-commercial use requires
+ *              attribution.
+ * @link		http://dmcbdesign.com
+ */
+class Autocomplete extends MY_Controller {
+
+	function Autocomplete()
+	{
+		parent::__construct();
+	}
+
+	function user()
+	{
+		$this->load->model('users_model');
+
+		$value = $this->input->post('displayname');
+		$choices = $this->users_model->autocomplete($value);
+
+		if ($choices->num_rows() > 0)
+		{
+			echo '<ul>';
+			foreach ($choices->result_array() as $choice)
+			{
+				echo '<li>'.$choice['displayname'].'</li>';
+			}
+			echo '</ul>';
+		}
+	}
+}
--- ci_2.0.3_application/views/form_page_permissions.php
+++ ci_2.0.3_application/views/form_page_permissions.php
@@ -1,27 +1,27 @@
 <form class="collapsible" action="<?=base_url();?><?=$page['urlname'];?>/permissions" method="post" onsubmit="return dmcb.submit(this);">
 	<fieldset>
 		<legend><a href="javascript:Effect.Combo('permissions');">Manage permissions</a></legend>
-		
+
 		<div id="permissions" class="panel"><div>
 			<?php if ($this->config->item('csrf_protection')) echo '<input type="hidden" name="'.$this->security->get_csrf_token_name().'" value="'.$this->security->get_csrf_hash().'" />';?>
 			<input type="hidden" name="buttonchoice" value="" class="hidden" />
-			
+
 			<?php
-				for ($i=0; $i<sizeof($userlist); $i++) 
+				for ($i=0; $i<sizeof($userlist); $i++)
 				{
 					if (sizeof($userlist[$i]) > 0)
 					{
 						$userrole = $roles->row_array($i);
 						echo '<div class="forminput"><label><td colspan="3">Page '.$userrole['role'].'s</label>';
 						echo '<table>';
-						for ($j=0; $j<sizeof($userlist[$i]); $j++) 
+						for ($j=0; $j<sizeof($userlist[$i]); $j++)
 						{
 							echo '<tr class="data"><td>'.$userlist[$i][$j]['displayname'].'</td>';
 
 							echo '<td><select onchange="dmcb.goto(this)" class="narrow">';
-							foreach ($roles->result_array() as $role) 
+							foreach ($roles->result_array() as $role)
 							{
-								if ($role['roleid'] == $userlist[$i][$j]['roleid']) 
+								if ($role['roleid'] == $userlist[$i][$j]['roleid'])
 								{
 									echo '<option value="'.base_url().$page['urlname'].'/permissions/set_role/'.$userlist[$i][$j]['userid'].'/'.$role['roleid'].'" selected="selected">'.$role['role'].'</option>';
 								}
@@ -38,23 +38,32 @@
 					}
 				}
 			?>
-		
+
 			<div class="forminput">
 				<label>User display name</label>
-				<input name="displayname" type="text" class="text" maxlength="20" value="<?php echo set_value('displayname'); ?>"/>
+				<input name="displayname" id="displayname" type="text" class="text" maxlength="20" value="<?php echo set_value('displayname'); ?>"/>
+				<div class="autocomplete" id="autocomplete" style="display: none; position:relative;"></div>
 				<?php echo form_error('displayname'); ?>
 			</div>
-			
+
+			<script type="text/javascript">
+				new Ajax.Autocompleter('displayname','autocomplete','<?php echo base_url();?>autocomplete/user', {
+					<?php if ($this->config->item('csrf_protection')) echo "parameters: '".$this->security->get_csrf_token_name()."=".$this->security->get_csrf_hash()."',";?>
+					minChars: 2,
+					frequency: 0.1
+				});
+			</script>
+
 			<div class="formnotes">
 				<p>If you are adding a new user to the site, in addition to their display name, enter their email address below. Otherwise ignore the email field and enter an existing user's display name.</p>
 			</div>
-			
+
 			<div class="forminput">
 				<label>Email address</label>
 				<input name="email" type="text" class="text" maxlength="50" value="<?php echo set_value('email'); ?>"/>
 				<?php echo form_error('email'); ?>
 			</div>
-			
+
 			<div class="forminput">
 				<label>Permission level</label>
 				<select name="role">
@@ -66,10 +75,10 @@
 				?>
 				</select>
 			</div>
-		
+
 			<div class="forminput">
 				<input type="submit" value="Set" name="set" class="button" onclick="dmcb.submitSetValue(this);" onfocus="dmcb.submitSetValue(this);" onblur="dmcb.submitRemoveValue(this);"/>
 			</div>
 		</div></div>
 	</fieldset>
-</form>	
+</form>
