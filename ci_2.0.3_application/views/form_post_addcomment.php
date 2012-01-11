31c217962c6e75eb251f23786f852864a4b896eb
Derek McBurney <d.mcburney@gmail.com>
2012-01-10 20:30:21 -0800

    Update Facebook authentication on Add Comment section.
    Facebook authentication has been updated to OAuth 2.0, and there's
    a special Add Comment login area that needed to be updated.

--- ci_2.0.3_application/views/form_post_addcomment.php
+++ ci_2.0.3_application/views/form_post_addcomment.php
@@ -1,7 +1,7 @@
 <form action="<?=base_url();?><?=$post['urlname'];?>/addcomment" method="post" onsubmit="return dmcb.submit(this);">
 	<fieldset>
 		<legend>Add a comment</legend>
-		
+
 		<div id="addcomment" class="panel alwaysopen"><div>
 			<?php if ($this->config->item('csrf_protection')) echo '<input type="hidden" name="'.$this->security->get_csrf_token_name().'" value="'.$this->security->get_csrf_hash().'" />';?>
 			<input type="hidden" name="buttonchoice" value="" class="hidden" />
@@ -10,78 +10,83 @@
 			if (!$this->session->userdata('signedon'))
 			{
 			?>
-			
+
 			<div class="formnotes full">
 				<p>You are not signed on, but you can comment. Your comment will need to be approved. To skip the wait for approval <a href="<?=base_url();?>signon/<?=$this->uri->segment(2);?>/addcomment/">sign on</a>.
 
 				<?php if ($this->config->item('dmcb_signon_facebook') == "true") { ?>
 				<span id="fb-root"></span>
 				<script>
-				  window.fbAsyncInit = function() {
-					FB.init({
-					  appId   : '<?php echo $this->config->item('dmcb_facebook_app_id'); ?>',
-					  session : <?php echo json_encode($session); ?>, // don't refetch the session when PHP already has it
-					  status  : true, // check login status
-					  cookie  : true, // enable cookies to allow the server to access the session
-					  xfbml   : true // parse XFBML
-					});
+					window.fbAsyncInit = function() {
+						FB.init({
+							appId   : '<?php echo $this->config->item('dmcb_facebook_app_id'); ?>',
+							oauth   : true,
+							authResponse : <?php echo json_encode($session); ?>, // don't refetch the session when PHP already has it
+							status  : true, // check login status
+							cookie  : true, // enable cookies to allow the server to access the session
+							xfbml   : true // parse XFBML
+						});
 
-					// whenever the user logs in, we refresh the page
-					FB.Event.subscribe('auth.login', function() {
-					  window.location.reload();
-					});
-				  };
+						// whenever the user logs in, we refresh the page
+						FB.Event.subscribe('auth.login', function() {
+							window.location.reload();
+						});
+						FB.Event.subscribe('auth.logout', function(response) {
+							window.location.reload();
+						});
+					};
 
-				  (function() {
-					var e = document.createElement('script');
-					e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-					e.async = true;
-					document.getElementById('fb-root').appendChild(e);
-				  }());
+					(function() {
+						var e = document.createElement('script');
+						e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+						e.async = true;
+						document.getElementById('fb-root').appendChild(e);
+					}());
 				</script>
+
 				<br/><br/>
-				<fb:login-button length="long" perms="email"></fb:login-button>
-				
+				<fb:login-button length="long" scope="email"></fb:login-button>
+
 				<?php } ?>
 
 				</p>
-			
+
 			</div>
-			
+
 			<input name="information" type="text" class="hidden" />
-			
+
 			<?php
 				if (form_error('email') != "<p class=\"error\">This email address is registered. Enter your account password to add your comment.</p>" && form_error('email') != "<p class=\"error\">Your password is incorrect.</p>")
 				{
 			?>
-			
+
 			<div class="forminput">
 				<label>Display name</label>
 				<input name="displayname" type="text" class="text" value="<?php echo set_value('displayname'); ?>"/>
 				<?php echo form_error('displayname'); ?>
 			</div>
-			
+
 			<?php
 				}
 			?>
-			
+
 			<div class="forminput">
 				<label>Email address</label>
 				<input name="email" type="text" class="text" value="<?php echo set_value('email'); ?>"/>
 				<?php echo form_error('email'); ?>
 			</div>
-			
+
 			<?php
 				if (form_error('email') == "<p class=\"error\">This email address is registered. Enter your account password to add your comment.</p>" || form_error('email') == "<p class=\"error\">Your password is incorrect.</p>")
 				{
 			?>
-			
+
 			<div class="forminput">
 				<label>Password</label>
 				<input name="password" type="password" class="text" />
 				<?php echo form_error('password'); ?>
 			</div>
-			
+
 			<?php
 				}
 			}
