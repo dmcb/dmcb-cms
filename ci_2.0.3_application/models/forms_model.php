8826238e8ec7bef75f03a0c140f6f9ced72910da
Derek McBurney <d.mcburney@gmail.com>
2012-01-18 22:30:16 -0800

    Repeated form submission limited by a minute, not a day.
    Repeated form submission currently limited to 1 day. This setting
    is a bit ridiculous, it's been limited to 1 minute instead.

--- ci_2.0.3_application/models/forms_model.php
+++ ci_2.0.3_application/models/forms_model.php
@@ -14,23 +14,23 @@ class Forms_model extends CI_Model {
     {
         parent::__construct();
     }
-	
+
 	function add($form, $email, $ip)
 	{
 		$date = date('Y-m-d H:i:s');
 		$this->db->query("INSERT into forms (form, email, ip, date) VALUES (".$this->db->escape($form).",".$this->db->escape($email).",".$this->db->escape($ip).",".$this->db->escape($date).")");
 	}
-	
+
 	function get_recent($email)
 	{
-		$query = $this->db->query("SELECT * FROM forms WHERE DATE_SUB(CURDATE(),INTERVAL 1 DAY) <= date AND email = ".$this->db->escape($email)." ORDER BY date DESC LIMIT 1");
+		$query = $this->db->query("SELECT * FROM forms WHERE DATE_SUB(CURDATE(),INTERVAL 1 MINUTE) <= date AND email = ".$this->db->escape($email)." ORDER BY date DESC LIMIT 1");
 		if ($query->num_rows() == 0)
 		{
 			return NULL;
 		}
 		else
 		{
-			return $query->row_array(); 
+			return $query->row_array();
 		}
 	}
 }
