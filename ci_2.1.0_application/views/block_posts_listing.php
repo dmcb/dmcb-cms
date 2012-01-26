9107943016e1bd993565c1572b84169886974d9f
Derek McBurney <d.mcburney@gmail.com>
2012-01-25 22:53:16 -0800

    Changing more views to use filename.X.Y.jpg sizing format.
    More views using size_image() function to adopt filename.X.Y.jpg
    format.

--- ci_2.1.0_application/views/block_posts_listing.php
+++ ci_2.1.0_application/views/block_posts_listing.php
@@ -2,17 +2,17 @@
 	<?php
 		if (isset($post['image']['urlpath']))
 		{
-			echo '<a href="'.base_url().$post['urlname'].'"><img src="'.base_url().$post['image']['urlpath'].'/120/80" alt="'.$post['title'].'" /></a>';
+			echo '<a href="'.base_url().$post['urlname'].'"><img src="'.base_url().size_image($post['image']['urlpath'],120,80).'" alt="'.$post['title'].'" /></a>';
 		}
 
 		echo '<h3>'.date("F j, Y", strtotime($post['date'])).'</h3>';
 		echo '<p><a href="'.base_url().$post['urlname'].'">'.$post['title'].'</a>';
-		
-		if ($post['needsubscription'] == "1" && $this->acl->enabled('site', 'subscribe')) 
+
+		if ($post['needsubscription'] == "1" && $this->acl->enabled('site', 'subscribe'))
 		{
 			echo '<span class="restricted">Subscription required</span>';
 		}
-		
+
 		if ($post['enabledcomments'])
 		{
 			if ($post['commentcount'] == 1) echo '<br/>'.$post['commentcount'].' comment';
--- ci_2.1.0_application/views/post_images.php
+++ ci_2.1.0_application/views/post_images.php
@@ -4,7 +4,7 @@ foreach ($images as $attached_image)
 	if ($attached_image['urlpath'] != $image['urlpath'])
 	{
 ?>
-		<a href="<?php echo base_url().$attached_image['urlpath'];?>" rel="lightbox[<?php echo $postid;?>]"><img src="<?php echo base_url().$attached_image['urlpath'];?>/40" alt="" class="listed_image"/></a>
+		<a href="<?php echo base_url().$attached_image['urlpath'];?>" rel="lightbox[<?php echo $postid;?>]"><img src="<?php echo base_url().size_image($attached_image['urlpath'],40);?>" alt="" class="listed_image"/></a>
 <?php
 	}
 }
