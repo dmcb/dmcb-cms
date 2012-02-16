f49036f7c084c743e5bce55cbe436e4603bb9557
Derek McBurney <d.mcburney@gmail.com>
2012-02-16 00:55:56 -0800

    Tagging user to a post uses autocomplete.
    The autocomplete user display name look up is now in use for
    tagging users to posts.

--- ci_2.1.0_application/views/form_post_taguser.php
+++ ci_2.1.0_application/views/form_post_taguser.php
@@ -1,11 +1,11 @@
 <form class="collapsible" action="<?=base_url();?><?=$post['urlname'];?>/taguser" method="post" onsubmit="return dmcb.submit(this);">
 	<fieldset>
 		<legend><a href="javascript:Effect.Combo('taguser');">Tag a user to post</a></legend>
-		
+
 		<div id="taguser" class="panel"><div>
 			<?php if ($this->config->item('csrf_protection')) echo '<input type="hidden" name="'.$this->security->get_csrf_token_name().'" value="'.$this->security->get_csrf_hash().'" />';?>
 			<input type="hidden" name="buttonchoice" value="" class="hidden" />
-		
+
 			<?php
 			for ($i=0; $i<=sizeof($contributors); $i++)
 			{
@@ -14,7 +14,7 @@
 					$contributor = $contributors[$i];
 				$field = 'contributor'.($i+1);
 				$field_error = 'contributor'.($i+1).'_error';
-				
+
 				echo '<div class="forminput">';
 				if ($i == sizeof($contributors) && $i != 0)
 					echo '<label>Tag another user</label>';
@@ -22,15 +22,25 @@
 					echo '<label>Tag a user</label>';
 				else
 					echo '<label>Tagged user #'.($i+1).'</label>';
-				echo '<input name="'.$field.'" type="text" maxlength="30" class="text" value="'.set_value($field, $contributor['displayname']).'"/>';
+				echo '<input name="'.$field.'" id="tag_'.$i.'" type="text" maxlength="30" class="text" value="'.set_value($field, $contributor['displayname']).'"/>';
+				echo '<div class="autocomplete" id="autocomplete_'.$i.'" style="display: none; position:relative;"></div>';
+
+				if ($this->config->item('csrf_protection')) $csrf = "parameters: '".$this->security->get_csrf_token_name()."=".$this->security->get_csrf_hash()."',";
+				$this->javascript['autocomplete'.$i] = array('weight' => 1, 'javascript' => "
+				new Ajax.Autocompleter('tag_".$i."','autocomplete_".$i."','".base_url()."autocomplete/user', {
+					".$csrf."
+					minChars: 2,
+					frequency: 0.1
+				});");
+
 				echo form_error($field);
 				echo '</div>';
 			}
 			?>
-		
+
 			<div class="forminput">
 				<input type="submit" value="Set" name="set" class="button" onclick="dmcb.submitSetValue(this);" onfocus="dmcb.submitSetValue(this);" onblur="dmcb.submitRemoveValue(this);"/>
 			</div>
 		</div></div>
 	</fieldset>
-</form>	
+</form>
