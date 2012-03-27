2bec4b9c3e5815d66e946e50e03ac3aea9ae8746
Derek McBurney <d.mcburney@gmail.com>
2012-03-27 00:28:53 -0700

    Tweaks to decrease accidental mailing list unsubscriptions.
    Accidental mailing list unsubscriptions were occuring. This
    decreases their likely hood by adding a confirmation button,
    as well as adds a link to reenable your subscription, and
    separating the unsubscribe link from the mailing list text
    more.

--- ci_2.1.0_application/controllers/unlist.php
+++ ci_2.1.0_application/controllers/unlist.php
@@ -13,6 +13,7 @@ class Unlist extends MY_Controller {
 	function Unlist()
 	{
 		parent::__construct();
+		$this->load->library('form_validation');
 	}
 
 	function _remap()
@@ -20,21 +21,33 @@ class Unlist extends MY_Controller {
 		$user = instantiate_library('user', $this->uri->segment(2));
 		if (isset($user->user['mailinglist_code']) && $user->user['mailinglist_code'] == $this->uri->segment(3))
 		{
-			$user->new_user['mailinglist'] = "0";
-			$user->save();
-			$data['subject'] = 'Success!';
-			$data['message'] = 'You have successfully unsubscribed from the '.$this->config->item('dmcb_friendly_server').' mailing list.';
+			$this->form_validation->set_rules('unsubscribe', 'unsubscribe', 'xss_clean|required');
 
-			$message = "You have successfully unsubscribed from the ".$this->config->item('dmcb_friendly_server')." mailing list.\n\nIf you feel this message is in error, please contact support at ".$this->config->item('dmcb_email_support');
+			if ($this->form_validation->run())
+			{
+				$user->new_user['mailinglist'] = "0";
+				$user->save();
+				$data['subject'] = 'Success!';
+				$data['message'] = 'You have successfully unsubscribed from the '.$this->config->item('dmcb_friendly_server').' mailing list.';
 
-			$this->load->model('notifications_model');
-			$this->notifications_model->send($user->user['email'], 'Unsubscribed from '.$this->config->item('dmcb_friendly_server').' mailing list', $message);
+				$message = "You have successfully unsubscribed from the ".$this->config->item('dmcb_friendly_server')." mailing list.\n\nIf you feel this message is in error, please contact support at ".$this->config->item('dmcb_email_support')."\n".
+					'If you would like to resubscribe, please visit the following link to set your mailing list settings: '.base_url().'account/messagesettings.';
+
+				$this->load->model('notifications_model');
+				$this->notifications_model->send($user->user['email'], 'Unsubscribed from '.$this->config->item('dmcb_friendly_server').' mailing list', $message);
+
+				$this->_message("Unlist account", $data['message'], $data['subject']);
+			}
+			else
+			{
+				$this->_initialize_page('unlist',"Unlist", NULL);
+			}
 		}
 		else
 		{
 			$data['subject'] = 'Error';
 			$data['message'] = 'Incorrect code. There was likely a problem with your unsubscribe link. Please visit the following link to set your mailing list settings: <a href="'.base_url().'account/messagesettings">'.base_url().'account/messagesettings</a>.';
+			$this->_message("Unlist account", $data['message'], $data['subject']);
 		}
-		$this->_message("Unlist account", $data['message'], $data['subject']);
 	}
 }
