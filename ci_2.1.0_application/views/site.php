7f4cc422910a1363f8c71aa2aa5b4734672d8f90
Derek McBurney <d.mcburney@gmail.com>
2012-11-10 16:34:26 -0800

    Moving tracking code into HEAD.
    That's the way Google rolls these days.

--- ci_2.1.0_application/views/site.php
+++ ci_2.1.0_application/views/site.php
@@ -15,26 +15,24 @@
 <?php if (isset($rss)) echo $rss; ?>
 <?php if (isset($packages)) echo $packages; ?>
 
+	<!-- google analytics -->
+	<script type="text/javascript">	
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', '<?php echo $this->config->item('dmcb_google_analytics_id');?>']);
+		_gaq.push(['_trackPageview']);
+		
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
 </head>
 <body>
 
 <?php if (isset($waiver)) echo $waiver; ?>
 <?php echo $site_content; ?>
 
-
-	<!-- google analytics -->
-	<script type="text/javascript">
-	
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', '<?php echo $this->config->item('dmcb_google_analytics_id');?>']);
-	_gaq.push(['_trackPageview']);
-	
-	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-	
-	</script>
 </body>
 </html>
